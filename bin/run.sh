a69704cb88c4568b5d692fa6614d275a9de1fa1a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-14 13:30:57 +0200

    Initial working version

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Synopsis:
 # Run the test runner on a solution.
@@ -24,6 +24,7 @@ fi
 slug="$1"
 input_dir="${2%/}"
 output_dir="${3%/}"
+tests_file="test_${slug}.R"
 results_file="${output_dir}/results.json"
 
 # Create the output directory if it doesn't exist
@@ -31,18 +32,33 @@ mkdir -p "${output_dir}"
 
 echo "${slug}: testing..."
 
+pushd "${input_dir}" > /dev/null
+
 # Run the tests for the provided implementation file and redirect stdout and
 # stderr to capture it
-# TODO: correct test command
-test_output=$(TEST_COMMAND 2>&1)
+test_output=$(Rscript "${tests_file}" 2>&1)
+exit_code=$?
+failed=$(echo "${test_output}" | grep -c -E '── (Failure|Error)')
+
+if [[ $exit_code -eq 0 ]] && [[ ! $failed -eq 0 ]]; then    
+    exit_code=1
+fi
+
+popd > /dev/null
 
 # Write the results.json file based on the exit code of the command that was 
 # just executed that tested the implementation file
-if [ $? -eq 0 ]; then
+if [[ $exit_code -eq 0 ]] && [ $failed -eq 0 ]; then
     jq -n '{version: 1, status: "pass"}' > ${results_file}
 else
-    # OPTIONAL: colors
-    jq -n --arg output "${test_output}" '{version: 1, status: "fail", output: $output}' > ${results_file}
+    # Sanitize the test output
+    sanitized_test_output=$(echo "${test_output}" | sed -E 's/🥇|🌈|🥳|🎊|😸|😀|🎉/🥇/g')
+
+    # Manually add colors to the output to help scanning the output for errors
+    colorized_test_output=$(echo "${sanitized_test_output}" | \
+        GREP_COLOR='01;31' grep --color=always -E -e '^── (Error|Failure).*|$')
+
+    jq -n --arg output "${colorized_test_output}" '{version: 1, status: "fail", output: $output}' > ${results_file}
 fi
 
 echo "${slug}: done"
