331e4606c80404ea78b69b2184bf968af72c9872
Jon Calder <jonmcalder@gmail.com>
2023-10-08 13:19:18 +0200

    :fire: Remove superfluous messages in tests

--- tests/example-all-fail/test_example-all-fail.R
+++ tests/example-all-fail/test_example-all-fail.R
@@ -1,8 +1,6 @@
 source("./example-all-fail.R")
 library(testthat)
 
-context("leap")
-
 test_that("year not divisible by 4: common year", {
   year <- 2015
   expect_equal(leap(year), FALSE)
@@ -12,5 +10,3 @@ test_that("year divisible by 4, not divisible by 100: leap year", {
   year <- 2016
   expect_equal(leap(year), TRUE)
 })
-
-message("All tests passed for exercise: leap")
