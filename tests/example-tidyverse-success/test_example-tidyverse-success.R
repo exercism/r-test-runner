5b79c0ae179d7d658f107095877d855f492ff541
Jon Calder <jonmcalder@gmail.com>
2023-10-09 11:25:40 +0200

    :fire: Remove superfluous messages in tests (#73)

--- tests/example-tidyverse-success/test_example-tidyverse-success.R
+++ tests/example-tidyverse-success/test_example-tidyverse-success.R
@@ -1,8 +1,6 @@
 source("./example-tidyverse-success.R")
 library(testthat)
 
-context("leap")
-
 test_that("year not divisible by 4: common year", {
   year <- 2015
   expect_equal(leap(year), FALSE)
@@ -22,5 +20,3 @@ test_that("year divisible by 400: leap year", {
   year <- 2000
   expect_equal(leap(year), TRUE)
 })
-
-message("All tests passed for exercise: leap")
